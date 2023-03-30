da2dd686dda0218eda2a7d7c79d91bb20f75875e
phucbm <hiphucbui@gmail.com>
2023-03-30 15:11:09 +0700

    build: update package.json

--- dev/script.js
+++ dev/script.js
@@ -17,7 +17,7 @@ const packageInfo = require('../package.json');
  */
 // update title
 const title = `${packageInfo.prettyName} v${packageInfo.version}`;
-document.title = `[DEV] ${title} - ${packageInfo.description}`;
+document.title = `${title} - ${packageInfo.description}`;
 document.querySelector('[data-title]').innerHTML = title;
 document.querySelector('[data-description]').innerHTML = packageInfo.description;
 
